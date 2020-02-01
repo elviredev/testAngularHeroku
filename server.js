3e862b8db60d44cef06a096b980eea7cd6208023
sand <sandrine.rodriguez@hotmail.com>
2020-02-01 17:06:01 +0100

    modif index.html

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.static('./dist/testAngularHeroku'));
+
 app.get('/*', function(req, res){
     res.sendfile(path.join(__dirname, '/dist/testAngularHeroku/index.html'));
 });
